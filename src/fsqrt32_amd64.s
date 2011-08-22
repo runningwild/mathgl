b0e1ec26701f7c8ddcf667aeb10f853d8874e138
Nils Hasenbanck <nils@hasenbanck.de>
2011-08-22 17:32:52 +0200

    fixed the assembler instruction for Fsqrt32
    Signed-off-by: Nils Hasenbanck <nils@hasenbanck.de>

--- src/fsqrt32_amd64.s
+++ src/fsqrt32_amd64.s
@@ -1,5 +1,5 @@
 // func Fsqrt32(x float32) float32
 TEXT ·Fsqrt32(SB),7,$0
-	SQRTSS x+0(FP), X0
-	MOVSD X0, r+8(FP)
+	SQRTSS x+0(FP), X0      // 0(FP) is the first argument, x is just a name
+	MOVSS X0, r+8(FP)       // 8(FP) is the return argument, r is just a name
 	RET
