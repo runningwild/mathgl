ac0fbc1f97bca4b3c567b47265a513dcfe83d7ed
Nils Hasenbanck <nils@hasenbanck.de>
2011-08-23 07:32:08 +0200

    Use only one test file and added test cases for Vec2.
    Signed-off-by: Nils Hasenbanck <nils@hasenbanck.de>

--- src/mathgl_test.go
+++ src/mathgl_test.go
@@ -22,8 +22,60 @@ func TestSqrtFloat32(t *testing.T) {
 		result = Fsqrt32(st.in)
 		error = ( Fmax32(st.out,result) / Fmin32(st.out,result) ) - 1
 		if error > 0.04 {
-		message := fmt.Sprintf("the error is too big: srqrt(%f) with error %f\n", st.in, error)
-		t.Errorf(message)
+		    message := fmt.Sprintf("The error is too big: srqrt(%f) with error %f\n", st.in, error)
+		    t.Errorf(message)
 		}
 	}
+}
+
+func TestVec2(t *testing.T) {
+    v2 := new(Vec2)
+	if v2.x != 0  || v2.y != 0 {
+		t.Errorf("Initialized Vec2 is not zero\n")
+	}
+	v2.Fill(4.0,3.0)
+	length := v2.Length()
+    if length != 5.0 {
+		message := fmt.Sprintf("Length of Vec2(4.0,3.0) should be 5.0 but is %f\n", length)
+		t.Errorf(message)
+	}
+	v2.Normalize()
+	length = v2.Length()
+	if length != 1.0 {
+		message := fmt.Sprintf("Length of Vec2(%f,%f) should be 1.0 but is %f\n", v2.x, v2.y, length)
+		t.Errorf(message)
+	}
+	v2.Add(v2)
+	if v2.x != 1.6 || v2.y != 1.2 {
+		message := fmt.Sprintf("Vec2(%f,%f) should be Vec2(1.6,1.2) after add function\n", v2.x, v2.y)
+		t.Errorf(message)
+	}
+	dot := v2.Dot(v2)
+	if dot != 4.0 {
+	    message := fmt.Sprintf("Dot product of Vec2(%f,%f) should be 4.0 but is %f \n", v2.x, v2.y, dot)
+		t.Errorf(message)
+	}
+	sub := new(Vec2)
+	sub.Fill(0.6,0.2)
+	v2.Subtract(sub)
+	if v2.x != 1.0 || v2.y != 1.0 {
+	    message := fmt.Sprintf("Vector should be Vec(1.0,1.0) but is Vec2(%f,%f) after subtraction\n", v2.x, v2.y)
+		t.Errorf(message)
+	}
+	identity := MakeIdentity()
+	v2.Transform(identity)
+	if v2.x != 1.0 || v2.y != 1.0 {
+	    message := fmt.Sprintf("Vector should be Vec(1.0,1.0) but is Vec2(%f,%f) after transformation with identity matrix\n", v2.x, v2.y)
+		t.Errorf(message)
+	}
+	v2.Scale(5.0)
+	if v2.x != 5.0 || v2.y != 5.0 {
+	    message := fmt.Sprintf("Vector should be Vec(5.0,5.0) but is Vec2(%f,%f) after scale with scalar 5.0\n", v2.x, v2.y)
+		t.Errorf(message)
+	}
+	if !v2.Areequal(v2) {
+	    message := fmt.Sprintf("Vector is not equal with himelf. We screwed up badly!", v2.x, v2.y)
+		t.Errorf(message)
+	}
+
 }
