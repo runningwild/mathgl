e39505c7a42e3622534bbbf8400fd1ec0a1559f1
Nils Hasenbanck <nils@hasenbanck.de>
2011-08-23 17:55:46 +0200

    Added some comments to support functions
    Signed-off-by: Nils Hasenbanck <nils@hasenbanck.de>

--- func.go
+++ func.go
@@ -1,5 +1,6 @@
 package mathgl
 
+// Some constants which are often in 3D math.
 const (
 	PI         float32 = 3.141592
 	PIover180  float32 = 0.017453
@@ -7,17 +8,22 @@ const (
 	epsilon    float32 = 1.0 / 64.0
 )
 
+// The Square of a given float32.
 func Fsqr32(s float32) float32 {
 	return s * s
 }
+
+// Returns the radius value from a given degree value given in float32.
 func Fdeg2rad32(degrees float32) float32 {
 	return degrees * PIover180
 }
 
+// Returns the degrees value from a given radius value given in float32.
 func Frad2deg32(radians float32) float32 {
 	return radians * PIunder180
 }
 
+// Returns the smallest value of two given float32 values.
 func Fmin32(lhs float32, rhs float32) float32 {
 	if lhs < rhs {
 		return lhs
@@ -25,6 +31,7 @@ func Fmin32(lhs float32, rhs float32) float32 {
 	return rhs
 }
 
+// Returns biggest value of two given float32 values.
 func Fmax32(lhs float32, rhs float32) float32 {
 	if lhs > rhs {
 		return lhs
@@ -32,6 +39,7 @@ func Fmax32(lhs float32, rhs float32) float32 {
 	return rhs
 }
 
+// Returns true if two float32 are almost the same (the threshold is epsilon = 1/64).
 func FalmostEqual32(lhs float32, rhs float32) bool {
 	return (lhs+epsilon > rhs && lhs-epsilon < rhs)
 }
